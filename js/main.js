adf7efb26447acf68f0e137b7b54caa43cd73a04
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2020-09-10 07:55:14 -0500

    new updates

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// Sticky menu background
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+$("#navbar a, .btn").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
+
+// Hover on touch devices
+
+(function () {
+  "use strict";
+
+  if (!("addEventListener" in window)) {
+    return;
+  }
+
+  var htmlElement = document.querySelector(".hover-active");
+
+  function touchStart() {
+    htmlElement.classList.remove("hover-active");
+
+    htmlElement.removeEventListener("touchstart", touchStart);
+  }
+
+  htmlElement.addEventListener("touchstart", touchStart);
+})();
